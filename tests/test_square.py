365ef051d1c78f25afe866844ca66702b5346f66
ACfma <andrea.carli1996@libero.it>
2020-10-19 16:57:42 +0200

    importing tests

---
+++ tests/test_square.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 16:23:36 2020
+
+@author: Andrea
+"""
+import unittest
+
+from Square.core import square
+
+class TestCore(unittest.TestCase):
+    '''Testing the core'''
+    def test_float(self):
+        '''Testing...'''
+        self.assertAlmostEqual(square(2.),4)
+
+
+if __name__=='__main__':
+    unittest.main()
+    
